efef2cdaa4c432f9f7d93ece0d275c508d1ec28e
Priyansh <priyansh.kushwaha_cs19@gla.ac.in>
2021-02-05 17:42:55 +0530

    Initial commit

---
+++ src/myinterface/MySinglyLinkedList.java
@@ -0,0 +1,12 @@
+package myinterface;
+
+public interface MySinglyLinkedList {
+    // insertAtBeginning
+    //insert ATHead
+    void addFirst(int element);
+    //insertAtEnd
+    //insertAtTail
+    //void addLast(int element);
+    boolean isEmpty();  // will give treu if linked List is empty
+    int size();  //will give totalnumber of element in linked list
+}
