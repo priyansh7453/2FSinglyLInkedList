a075ad3cf606ffc855a5ee6e7fdf61f2439d8d69
Priyansh <priyansh.kushwaha_cs19@gla.ac.in>
2021-02-10 10:58:36 +0530

    initial commit

--- src/myinterface/MySinglyLinkedList.java
+++ src/myinterface/MySinglyLinkedList.java
@@ -9,4 +9,15 @@ public interface MySinglyLinkedList {
     //void addLast(int element);
     boolean isEmpty();  // will give treu if linked List is empty
     int size();  //will give totalnumber of element in linked list
+    void addLast(int element);
+    void addLastWithoutUsingTail(int element);
+    void traverse();
+    void addAfterGivenNode(int givenElement , int element);
+    //void addBeforeGivenNode(int givenElement , int element);
+    int removeFirst();
+    int removeLast();
+    int removeGivenNode(int givenElement);
+    boolean searchElement(int element);
+
+
 }
